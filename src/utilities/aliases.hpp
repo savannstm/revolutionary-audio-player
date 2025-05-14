251b7424fa3122c16f86b2b7b5e35ee8bf2a9bfb
savannstm <savannstm@gmail.com>
2025-05-14 18:27:12 +0500

    Improve aliases, add `range` alias for views::iota

--- src/utilities/aliases.hpp
+++ src/utilities/aliases.hpp
@@ -21,6 +21,17 @@ template <typename O, typename T>
     return static_cast<O>(std::forward<T>(arg));
 }
 
+using namespace Qt::Literals::StringLiterals;
+using namespace std::literals::string_view_literals;
+
+namespace fs = std::filesystem;
+namespace views = std::views;
+namespace ranges = std::ranges;
+
+using qi64 = qint64;
+using qi32 = qint32;
+using qu64 = quint64;
+
 using usize = std::size_t;
 using isize = std::intptr_t;
 using u8 = std::uint8_t;
@@ -36,14 +47,6 @@ using f64 = double;
 using str = char*;
 using cstr = const char*;
 
-using qi64 = qint64;
-using qi32 = qint32;
-using qu64 = quint64;
-
-namespace fs = std::filesystem;
-namespace views = std::views;
-namespace ranges = std::ranges;
-
 using fs::path;
 using std::abs;
 using std::array;
@@ -51,39 +54,34 @@ using std::cerr;
 using std::clamp;
 using std::cout;
 using std::format;
+using std::make_shared;
 using std::make_unique;
 using std::map;
 using std::max;
 using std::min;
 using std::println;
 using std::set;
+using std::shared_ptr;
+using std::span;
 using std::string;
 using std::string_view;
 using std::to_string;
-using std::tuple;
-using std::vector;
-
-template <typename T, typename E>
-using result = std::expected<T, E>;
-
-template <typename E>
-using err = std::unexpected<E>;
-
 using std::tolower;
 using std::toupper;
+using std::tuple;
+using std::unique_ptr;
+using std::vector;
 
 using panic = std::runtime_error;
-
-using std::shared_ptr;
-using std::unique_ptr;
-
-using std::make_shared;
-using std::make_unique;
-
+template <typename T, typename E>
+using result = std::expected<T, E>;
+template <typename E>
+using err = std::unexpected<E>;
 template <typename K, typename V>
 using hashmap = std::unordered_map<K, V>;
 template <typename K>
 using hashset = std::unordered_set<K>;
 
-using namespace Qt::Literals::StringLiterals;
-using namespace std::literals::string_view_literals;
+constexpr auto range(usize from, usize dest) {
+    return views::iota(from, dest);
+}
