579170ad4519029b32f33980c9bbff0436b48447
savannstm <savannstm@gmail.com>
2025-11-20 20:21:31 +0500

    Fix PRNG

--- src/utilities/RandInt.hpp
+++ src/utilities/RandInt.hpp
@@ -2,25 +2,31 @@
 
 #include "Aliases.hpp"
 
-// NOLINTBEGIN: these aren't magic numbers, just xor-shift prng
-inline auto randint(u32 seed) -> u32 {
+static thread_local u32 seed =
+    duration_cast<std::chrono::milliseconds>(
+        std::chrono::system_clock::now().time_since_epoch()
+    )
+        .count();
+
+// NOLINTBEGIN: xorshift32
+inline auto randint() -> u32 {
     seed ^= seed << 13;
     seed ^= seed >> 17;
     seed ^= seed << 5;
     return seed;
 }
 
-// NOLINTEND
-
-inline auto randint_range(
-    const u32 min,
-    const u32 max,  // NOLINT: not so easily swappable
-    const u32 seed
-) -> u32 {
-    if (min == max) {
-        return min;
-    }
-
+inline auto randint_range(const u32 min, const u32 max) -> u32 {
     const u32 range = max - min + 1;
-    return min + (randint(seed) % range);
+    const u32 limit = UINT32_MAX - (UINT32_MAX % range);
+
+    u32 x;
+
+    do {
+        x = randint();
+    } while (x >= limit);
+
+    return min + (x % range);
 }
+
+// NOLINTEND
